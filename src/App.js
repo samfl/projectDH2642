cc64a04e69e42787627e5e4cff75235d87eb46e5
LucasVillarroel <vilucas7@gmail.com>
2019-12-16 17:18:32 +0100

    home page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            title: "BOOLU",
+            title: "BỌỌLU",
         };
     }
 
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,9 +6,28 @@ class Home extends Component {
     render() {
         return (
             <div className="Home">
-                hejhej
+                <h1 id={"home-title"}>BỌỌLU</h1>
+                <div id={"home-buttons"}>
+                    <a onClick={this.signIn} id={"home-signUp"}>Sign up</a>
+                    <a id={"home-logIn"}>Log in</a>
+                </div>
+                <div id={"home-signUp-info"}>
+                    Username<input className={"home-input"} type={"text"}/><br/>
+                    Password<input className={"home-input"} type={"text"}/><br/>
+                    Favorite team <input className={"home-input"} type={"text"}/> <button>search</button><br/>
+                    <button>submit</button>
+                </div>
             </div>
         );
+    }
+    signIn = e => {
+        let home = e.target.parentElement.parentElement;
+        let title = e.target.parentElement.parentElement.firstElementChild;
+        let signUp = e.target.parentElement.parentElement.firstElementChild.nextElementSibling.nextElementSibling;
+        console.log(signUp);
+        home.setAttribute('style', 'justify-content: normal');
+        title.setAttribute('style', 'font-size: 10vw');
+        signUp.setAttribute('style', 'display: inline; opacity: 1');
     }
 }
 
