c7203838b29578c41aece4a14403a2fa876ee0f7
LucasVillarroel <vilucas7@gmail.com>
2020-01-06 12:28:42 +0100

    added more leagues to search

--- client/src/components/search/search.js
+++ client/src/components/search/search.js
@@ -26,9 +26,13 @@ class Search extends Component {
                             <input type={"text"} placeholder={"Enter a key word"} id={"search-query"}/>
                             <select id={"search-select"} onChange={this.onLeagueSelect}>
                                 <option value="PL">Premier League</option>
+                                <option value="ELC">Championship</option>
                                 <option value="PD">La Liga</option>
                                 <option value="SA">Serie A</option>
-                                <option value="BL1">Bundesliga</option>
+                                <option value="BL1">1. Bundesliga</option>
+                                <option value="FL1">Ligue 1</option>
+                                <option value="DED">Eredivisie</option>
+                                <option value="PPL">Primeira Liga</option>
                             </select>
                             <button value={this.props.search.query}
                                     onClick={this.onSearch}>search</button>
