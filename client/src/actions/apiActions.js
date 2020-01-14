090abb64896ccf1c5a9ac9c3ae4565a110f93e99
LucasVillarroel <vilucas7@gmail.com>
2020-01-14 16:22:12 +0100

    frontend gets config vars from backend

--- client/src/actions/apiActions.js
+++ client/src/actions/apiActions.js
@@ -11,44 +11,36 @@ import {
     SCHEDULE_LOADING,
     FOCUS_CHANGED
 } from "../actions/types";
-require('dotenv').config();
+
 const BASE_URL = "https://api.football-data.org/v2";
 const proxyURL = 'https://cors-anywhere.herokuapp.com/';
-const httpOptions = {
-    method: "GET",
-    headers: {
-        "X-Auth-Token": "8ea8195d0b014e859507577cdbfe683d",
-        "X-Requested-With": "\"\""
-    },
-    redirect: "follow"
-};
 
 export const getTeams = (league, query) =>{
     return function(dispatch){
         dispatch({
             type: TEAMS_LOADING
         });
-        const targetURL = `${BASE_URL}/competitions/${league}/teams`;
         axios
             .get('/configvars')
             .then(res => {
-                console.log(res);
-            });
-        fetch(proxyURL + targetURL, httpOptions)
-            .then(response => response.json())
-            .then(response =>{
-            dispatch({
-                type: TEAMS_LOADED,
-                payload: {
-                    league: league,
-                    query: query,
-                    teams: response.teams.filter(teams => teams.name.toUpperCase().includes(query.toUpperCase())),
-                    isLoading: false
-                    }
-                });
-            })
-            .catch(err => {
-                console.log('ERROR', err);
+                const config = JSON.parse(res.data);
+                const targetURL = `${BASE_URL}/competitions/${league}/teams`;
+                fetch(proxyURL + targetURL, config)
+                    .then(response => response.json())
+                    .then(response =>{
+                        dispatch({
+                            type: TEAMS_LOADED,
+                            payload: {
+                                league: league,
+                                query: query,
+                                teams: response.teams.filter(teams => teams.name.toUpperCase().includes(query.toUpperCase())),
+                                isLoading: false
+                            }
+                        });
+                    })
+                    .catch(err => {
+                        console.log('ERROR', err);
+                    });
             });
     }
 };
@@ -58,22 +50,27 @@ export const getStandings = (league, season='2019') =>{
         dispatch({
             type: STANDINGS_LOADING
         });
-        const targetURL = `${BASE_URL}/competitions/${league}/standings?season=${season}`
-        fetch(proxyURL + targetURL, httpOptions)
-            .then(response => response.json())
-            .then(response =>{
-                dispatch({
-                    type: STANDINGS_LOADED,
-                    payload: {
-                        table: response.standings[0].table,
-                        homeTable: response.standings[1].table,
-                        awayTable: response.standings[2].table
-                    }
-                })
+        axios
+            .get('/configvars')
+            .then(res =>{
+                const config = JSON.parse(res.data);
+                const targetURL = `${BASE_URL}/competitions/${league}/standings?season=${season}`
+                fetch(proxyURL + targetURL, config)
+                    .then(response => response.json())
+                    .then(response =>{
+                        dispatch({
+                            type: STANDINGS_LOADED,
+                            payload: {
+                                table: response.standings[0].table,
+                                homeTable: response.standings[1].table,
+                                awayTable: response.standings[2].table
+                            }
+                        })
+                    })
+                    .catch(err => {
+                        console.log('ERROR', err);
+                    });
             })
-            .catch(err => {
-                console.log('ERROR', err);
-            });
     }
 };
 
@@ -82,19 +79,23 @@ export const getTopScorers = (league, season='2019') =>{
         dispatch({
             type: SCORERS_LOADING
         });
-        const targetURL = `${BASE_URL}/competitions/${league}/scorers?season=${season}`
-        fetch(proxyURL + targetURL, httpOptions)
-            .then(response => response.json())
-            .then(response =>{
-                dispatch({
-                    type: SCORERS_LOADED,
-                    payload: response.scorers
-                })
+        axios
+            .get('/configvars')
+            .then(res =>{
+                const config = JSON.parse(res.data);
+                const targetURL = `${BASE_URL}/competitions/${league}/scorers?season=${season}`
+                fetch(proxyURL + targetURL, config)
+                    .then(response => response.json())
+                    .then(response =>{
+                        dispatch({
+                            type: SCORERS_LOADED,
+                            payload: response.scorers
+                        })
+                    })
+                    .catch(err => {
+                        console.log('ERROR', err);
+                    });
             })
-            .catch(err => {
-                console.log('ERROR', err);
-            });
-
     }
 };
 
@@ -103,19 +104,24 @@ export const getSchedule = (teamId, league, season='2019') =>{
         dispatch({
             type: SCHEDULE_LOADING
         });
-        const targetURL = `${BASE_URL}/competitions/${league}/matches?season=${season}`;
-        fetch(proxyURL + targetURL, httpOptions)
-            .then(response => response.json())
-            .then(response => {
-                dispatch({
-                    type: SCHEDULE_LOADED,
-                    payload: response.matches.filter(matches =>
-                        (matches.homeTeam.id == teamId) || (matches.awayTeam.id == teamId))
-                })
+        axios
+            .get('/congigvars')
+            .then(res =>{
+                const config = JSON.parse(res.data);
+                const targetURL = `${BASE_URL}/competitions/${league}/matches?season=${season}`;
+                fetch(proxyURL + targetURL, config)
+                    .then(response => response.json())
+                    .then(response => {
+                        dispatch({
+                            type: SCHEDULE_LOADED,
+                            payload: response.matches.filter(matches =>
+                                (matches.homeTeam.id == teamId) || (matches.awayTeam.id == teamId))
+                        })
+                    })
+                    .catch(err => {
+                        console.log('ERROR', err);
+                    });
             })
-            .catch(err => {
-                console.log('ERROR', err);
-            });
     }
 };
 
