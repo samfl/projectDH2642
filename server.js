1edaebb83eb4c20905a26510bcbda70d75742a1e
LucasVillarroel <vilucas7@gmail.com>
2020-01-28 10:57:12 +0100

    minor change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get('*', (request, response) => {
+  app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
